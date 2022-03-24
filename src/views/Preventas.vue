f98aaaad2b95c429e8402179a738dec6799635a6
KarenAlvarez-19 <90125906+KarenAlvarez-19@users.noreply.github.com>
2022-03-24 16:17:59 -0600

    mensaje

---
+++ src/views/Preventas.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app>
+        <v-row>
+            <v-col cols="8">
+                <v-row>
+                    <v-col style="background:green">
+                        <v-img 
+                        src="../../src/assets/lorax.jpg"
+                        width="180"
+                        height="280"
+                        class="text-center"
+                        />
+                    </v-col>
+                    <v-col style="background:pink">
+                        <v-img 
+                        src="../../src/assets/dragon.jpg"
+                        width="180"
+                        height="280"
+                        />
+                    </v-col>
+                    <v-col style="background:green">
+                        <v-img 
+                        src="../../src/assets/posterGrud.png"
+                        width="180"
+                        height="280"
+                        />
+                    </v-col>
+                    <v-col style="background:pink">
+                        <v-img 
+                        src="../../src/assets/posterCastillo.jpeg"
+                        width="180"
+                        height="280"
+                        />
+                    </v-col>
+                </v-row>
+            </v-col>
+            <v-col cols="4" style="background:green"></v-col>
+        </v-row>
+    </v-app>
+</template>
