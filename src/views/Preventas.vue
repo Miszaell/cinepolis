218c9a66d0454bdf5b45d3bbe63586a420eb8147
miszaell <misaelvgm011@gmail.com>
2022-03-24 18:55:36 -0600

    add

--- src/views/Preventas.vue
+++ src/views/Preventas.vue
@@ -40,7 +40,7 @@
             /> </v-col
           >s
         </v-row>
-        <v-row class="px-lg-5">
+        <v-row class="px-lg-5 mt-lg-10">
           <v-col style="background: white">
             <v-text>Cinépolis VIP</v-text>
           </v-col>
@@ -85,48 +85,117 @@
       </v-col>
 
       <v-col cols="4">
-        <v-row
-          ><v-card
-            style="background: #002069"
-            class="mx-auto my-lg-5"
-            max-width="344"
-          >
+          <v-card class="mx-auto my-lg-5" max-width="344" color="#002069">
+            <v-card>
+              <v-card-title>
+                <v-icon>mdi-map-marker-radius-outline</v-icon>
+                Complejos
+              </v-card-title>
+            </v-card>
             <v-card-text>
-              <v-select :items="items" label="Ciudad" solo></v-select>
-              <v-select :items="items" label="Cine" solo></v-select>
-            </v-card-text> </v-card
-        ></v-row>
-        <v-row
-          ><v-card
-            style="background: #002069"
-            class="mx-auto my-lg-5"
-            max-width="344"
-          >
-            <v-card-text>
+              <v-autocomplete
+                chips
+                clearable
+                hide-details
+                hide-selected
+                item-text="name"
+                item-value="symbol"
+                label="Ubicación"
+                class="my-lg-3"
+                solo
+              >
+                <template v-slot:no-data>
+                  <v-list-item>
+                    <v-list-item-title> Sin resultados </v-list-item-title>
+                  </v-list-item>
+                </template>
+              </v-autocomplete>
+              <v-autocomplete
+                chips
+                clearable
+                hide-details
+                hide-selected
+                item-text="name"
+                item-value="symbol"
+                label="Ubicación"
+                class="my-lg-3"
+                solo
+              >
+                <template v-slot:no-data>
+                  <v-list-item>
+                    <v-list-item-title> Sin resultados </v-list-item-title>
+                  </v-list-item>
+                </template>
+              </v-autocomplete>
+            </v-card-text>
+          </v-card>
+          <v-card class="mx-auto my-lg-5" max-width="344" color="#002069">
+            <v-card>
+              <v-card-title>
+                <v-icon>mdi-clock-time-five-outline</v-icon>
+                Horario
+              </v-card-title>
+            </v-card>
+            <v-card-text class="text-center">
               <v-slider
                 v-model="value"
                 step="10"
                 ticks="always"
+                color="white"
                 tick-size="4"
               ></v-slider>
-            </v-card-text> </v-card
-        ></v-row>
-        <v-row
-          ><v-card
-            style="background: #002069"
-            class="mx-auto my-lg-5"
-            max-width="344"
-          >
+              <v-subheader dark class="mx-lg-auto text-center"
+                >00:00:00 - 00:00:00</v-subheader
+              >
+            </v-card-text>
+          </v-card>
+          <v-card class="mx-auto my-lg-5" max-width="344" color="#002069">
+            <v-card>
+              <v-card-title>
+                <v-icon>mdi-calendar-blank-outline</v-icon>
+                Día
+              </v-card-title>
+            </v-card>
             <v-card-text>
-              <v-select :items="items" label="23 Marzo" solo></v-select>
-            </v-card-text> </v-card
-        ></v-row>
-        <v-row
-          ><v-card
-            style="background: #002069"
-            class="mx-auto my-lg-5"
-            max-width="344"
-          >
+              <v-menu
+                ref="menu"
+                v-model="menu2"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="time"
+                transition="scale-transition"
+                offset-y
+                dark
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="time"
+                    label="Picker in menu"
+                    prepend-icon="mdi-clock-time-four-outline"
+                    readonly
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  v-if="menu2"
+                  v-model="time"
+                  full-width
+                  @click:minute="$refs.menu.save(time)"
+                ></v-time-picker>
+              </v-menu>
+            </v-card-text>
+          </v-card>
+          <v-card class="mx-auto my-lg-5" max-width="344" color="#002069">
+            <v-card>
+              <v-card-title>
+                <v-icon>mdi-movie</v-icon>
+                Tipo de proyección
+              </v-card-title>
+            </v-card>
             <v-card-text>
               <p style="color: white; margin-top: 50px">Experiencia</p>
               <v-checkbox
@@ -165,25 +234,20 @@
                 dark
                 hide-details
               ></v-checkbox>
-            </v-card-text> </v-card
-        ></v-row>
+            </v-card-text>
+          </v-card>
       </v-col>
     </v-row>
   </v-app>
 </template>
 <script>
   export default {
-    data () {
+    data() {
       return {
         value: 0,
         fruits: 0,
-        ticksLabels: [
-          'Figs',
-          'Lemon',
-          'Pear',
-          'Apple',
-        ],
-      }
+        ticksLabels: ["Figs", "Lemon", "Pear", "Apple"],
+      };
     },
-  }
+  };
 </script>
